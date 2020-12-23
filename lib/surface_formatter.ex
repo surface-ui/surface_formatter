8f7e3c35832f025a77bca5b8a58b9ed6bef49a6b
Paul Statezny <paulstatezny@gmail.com>
2020-12-23 10:29:05 -0700

    Remove boilerplate

--- lib/surface_formatter.ex
+++ lib/surface_formatter.ex
@@ -1,18 +1,2 @@
 defmodule SurfaceFormatter do
-  @moduledoc """
-  Documentation for `SurfaceFormatter`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SurfaceFormatter.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
